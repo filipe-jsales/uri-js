b675b965796b772f488fce785d8a9c6f45ca96d8
Filipe Sales <eduardojs999@gmail.com>
2022-08-01 11:49:38 -0300

    1096-js

---
+++ 1096-js/1096.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+for(var i=1; i<=9; i+=2){
+    // console.log(i)
+    j = 7
+    if(i%2==1){
+        while(j>4){
+            console.log(`I=${i} J=${j}`)
+            // console.log("j"+ j)
+            j--
+        }
+    }
+    
+}
