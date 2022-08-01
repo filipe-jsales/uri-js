43c994795e97e17bf7296c0e9a9e94ab255924a5
Filipe Sales <eduardojs999@gmail.com>
2022-08-01 10:12:38 -0300

    1044

---
+++ 1044-js/1044.js
@@ -0,0 +1,25 @@
+
+const lines = prompt('Digite os valores de entrada').split(' ') //testar no html
+
+var [A,B] = lines.map(Number);
+
+if ((A%B ===0) || (B%A ===0)){
+    console.log("Sao Multiplos");
+}else{
+    console.log("Nao sao Multiplos");
+}
+
+
+
+
+
+
+//const abdc = lines.shift().split(" ")
+//const a = parseInt(abdc.shift());
+
+//const b = parseInt(abdc.shift());
+
+//const c = parseInt(abdc.shift());
+
+//const d = parseInt(abdc.shift());
+
